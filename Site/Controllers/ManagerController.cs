577093f9a9b8c7a861b37e2644d7ebb3b839c7d6
edmilson souza da silva <edmilson.silva_desenv@hotmail.com>
2016-10-13 09:09:56 -0300

    primeiro commit

---
+++ Site/Controllers/ManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public PartialViewResult Iniciar()
+        {
+            return PartialView("_partial");
+        }
+    }
+}
---
+++ Site/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Site.App_Start;
+
+namespace Site
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
